e465d995eca9f5ededdd5f53ff20d0cb2477e86a
Bazell Partridge <elbitapmoC@users.noreply.github.com>
2023-08-17 14:44:40 -0400

    save progress

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 
-import "../globals.css";
+import "@/app/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,16 +1,15 @@
-import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import { dark } from "@clerk/themes";
 
-import Topbar from "../components/shared/Topbar";
-import Bottombar from "../components/shared/Bottombar";
-import LeftSidebar from "../components/shared/LeftSidebar";
-import RightSidebar from "../components/shared/RightSidebar";
+import Topbar from "@/app/components/shared/Topbar";
+import Bottombar from "@/app/components/shared/Bottombar";
+import LeftSidebar from "@/app/components/shared/LeftSidebar";
+import RightSidebar from "@/app/components/shared/RightSidebar";
 
-import "../globals.css";
+import "@/app/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
